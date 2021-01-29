a82fdb1ba6341234bd0b57da7469c15a80d7bdf8
HelyeFab <HF>
2021-01-29 19:21:40 +0000

    addetto news script

---
+++ app.js
@@ -0,0 +1,12 @@
+let min=1,
+max = 10,
+winningNum= 7;
+
+const minNum= document.querySelector('.min-num')
+const maxNum= document.querySelector('.max-num')
+
+
+
+
+minNum.textContent = min
+maxNum.textContent = max
